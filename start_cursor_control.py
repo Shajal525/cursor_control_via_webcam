511d7ab9a4c8eef6e852aea18b4ddb086fcb1e36
Shaheduzzaman Shajal <shajal525@gmail.com>
2021-02-15 19:53:27 +0600

    changed default source to 0

--- start_cursor_control.py
+++ start_cursor_control.py
@@ -173,7 +173,7 @@ def detect(save_img=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='pretrained_custom_model/Latest_model.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='http://192.168.31.69:4747/video', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default='0', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
